977067206f2cf402314309700958b9c3b1dd5f5a
LiorionNguyen <liorion.nguyen@gmail.com>
2024-11-22 20:59:53 +0700

    BTVN: Navigation

---
+++ src/data/user.ts
@@ -0,0 +1,22 @@
+// Tạo 5 account có dạng data như dưới.
+
+export const users = [
+    {
+        email: 'john@doe.com',
+        fullName: 'John Doe',
+        password: '123456',
+        gender: 'male',
+        age: 20,
+        address: '123 Main St, Anytown, USA',
+        status: 'active'
+    },
+    {
+        email: 'chung@gmail.com',
+        fullName: 'Nguyen Quoc Chung',
+        password: 'chung123',
+        gender: 'male',
+        age: 20,
+        address: '123 Main St, Anytown, USA',
+        status: 'inactive'
+    }
+]
