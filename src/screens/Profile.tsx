d087826a06d4f8d4354d1a5aeea941b0562ccc4c
LiorionNguyen <liorion.nguyen@gmail.com>
2024-12-27 20:58:40 +0700

    updat

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 export default function Profile() {
     const { user } = useSelector((state: any) => state.user); // Get value state
     console.log("User: ", user);
-    return <View>
+    return <View style={styles.container}>
         <Text>Profile</Text>
         {
             user.profileImage ? <Image source={{ uri: user.profileImage }} style={styles.avatar} />
@@ -18,12 +18,34 @@ export default function Profile() {
         <Text>Date Of Birth: {user.dateOfBirth ? user.dateOfBirth : "chua co thong tin"}</Text>
         <Text>Address: {user.address ? user.address : "chua co thong tin"}</Text>
         <Text>Role: {user.role ? user.role : "chua co thong tin"}</Text>
+        <Text style={styles.status}>Status: {user.isActive ? "Active" : "InActive"}</Text>
     </View>
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        gap: 20,
+        padding: 20
+    },
     avatar: {
         width: 150,
-        height: 150
+        height: 150, 
+    },
+    status: {
+        backgroundColor: "green",
+        width: 200,
+        textAlign: "center",
+        color: "white",
+        fontSize: 18,
+        fontWeight: 600,
+        padding: 10,
+        borderWidth: 1,
+        borderColor: "red",
+        borderRadius: 20,
+        // marginTop: 20
     }
 })
+
+// Content, Padding, Border, Margin
